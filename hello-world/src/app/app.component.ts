3faf0daf6cfc6902fd76eb2b9842a49880bc97a6
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-09-20 11:11:02 +0500

    import components in app.component.ts

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoursesComponent } from './courses.component';
+import { SearchBarComponent } from './searchBar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoursesComponent, SearchBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Lone Wolf';
 }
---
+++ hello-world/src/app/searchBar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'searchbar',
+    templateUrl: './template/searchbar.template.html',
+    standalone: true
+})
+
+export class SearchBarComponent {}
